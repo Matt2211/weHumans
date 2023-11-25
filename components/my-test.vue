de924e0dc516c3f46f0d5047cda582046812e6aa
Matt <matteofelicidesign@gmail.com>
2023-11-25 04:54:42 +0000

    initial

---
+++ components/my-test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+       <h1>ciao</h1> hello
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+ <MyTest />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
